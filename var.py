eb8389d453144f6bd5489eb355de6a907472b57a
Dimaniha <89929501+Dimaniha@users.noreply.github.com>
2022-04-26 23:03:55 +0300

    Delete var.py

--- var.py
+++
@@ -1,7 +0,0 @@
-API_TOKEN = '1950075219:AAFC3F0gXxMDO6w-5V8PqGBvkldRxV-87Ds'
-feedback_screenshot = 'F:/Рабочий стол/python/bet365simple/feedback_screenshot.jpg'
-uid = '565408648' #'-1001342986108' #id чата\пользователя для отправки скрина
-bot_number = 'bot1' #msg from a bot
-paswd = 'chafTey95'
-video_path = 'C:/Users/User/Documents/Bandicam/'
-start_video_hotkey = 'f9'
